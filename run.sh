388328d76b62627ebca77ab43489508dc4878772
cookiekop <michaelzhang1234@gmail.com>
2022-04-01 23:24:50 +0800

    Update: python

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-python3 update_db.py
+python update_db.py
 download_dir=`python3 get_magnet_url.py`
 aria2c -i magnets.txt -d $download_dir -l ariac.log --listen-port=6881 --dht-listen-port=6881
-python3 clean_up.py
+python clean_up.py
