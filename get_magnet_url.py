6c09071058718fceab98f943484ced26dd560b97
cookiekop <michaelzhang1234@gmail.com>
2022-04-02 11:43:49 +0800

    Update: separate

--- get_magnet_url.py
+++ get_magnet_url.py
@@ -6,6 +6,7 @@ import sys
 
 handler = ErrorHandler()
 def main():
+
     client = api.RarbgAPI(options={'retries': 1})
     db = MediaSQL()
     movie_categories = [api.RarbgAPI.CATEGORY_MOVIE_X264, \
@@ -13,22 +14,21 @@ def main():
                         api.RarbgAPI.CATEGORY_MOVIE_X264_720P, \
                         api.RarbgAPI.CATEGORY_MOVIE_X265_1080P]
     series_categories = [api.RarbgAPI.CATEGORY_TV_EPISODES_HD]
-    db_data = db.query(db_tb_name, "(rating=0 OR rating=5) AND jf_id is NULL")[:max_downloads]
-    links, names = [], []
-    for data in db_data:
-        category = series_categories if data[2] == 1 else movie_categories
-        for retry in range(retries):
-            api_ret = client.search(search_imdb=data[0], categories=category, sort='last')
-            if len(api_ret) > 0: 
-                names.append(api_ret[0].filename)
-                links.append(api_ret[0].download)
-                break
-            if (retry == retries - 1): handler.notify("RARBG Max Retries")
-    with open("magnets.txt", "w") as f_l, open("filenames.txt", "w") as f_n:
-        for i, (link, name) in enumerate(zip(links, names)): 
-            last_c = "\n" if i != max_downloads - 1 else ""
-            f_l.write(link + last_c)
-            f_n.write(name + last_c)
+    db_data = db.query(db_tb_name, "(rating=0 OR rating=5) AND jf_id is NULL AND is_series=0")[:max_downloads]
+    with open("magnets_movies.txt", "w") as f_l_m, \
+         open("magnets_series.txt", "w") as f_l_s, \
+         open("filenames.txt", "w") as f_n:
+        for data in db_data:
+            category = series_categories if data[2] == 1 else movie_categories
+            f = f_l_s if data[2] == 1 else f_l_m
+            fn_prefix = "series" if data[2] == 1 else "movies"
+            for retry in range(retries):
+                api_ret = client.search(search_imdb=data[0], categories=category, sort='last')
+                if len(api_ret) > 0: 
+                    f_n.write(fn_prefix+"/"+api_ret[0].filename+"\n")
+                    f.write(api_ret[0].download+"\n")
+                    break
+                if (retry == retries - 1): handler.notify("RARBG Max Retries")
     print(download_dir)
     
 
--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ export PYTHONIOENCODING=utf-8
 
 python update_db.py
 download_dir=`python3 get_magnet_url.py`
-aria2c -c -i magnets.txt -d $download_dir -l ariac.log --listen-port=6881 --dht-listen-port=6881
+aria2c -c -i magnets_movies.txt -d ${download_dir}/movie -l ariac.log --listen-port=16999 --dht-listen-port=16999
+aria2c -c -i magnets_series.txt -d ${download_dir}/series -l ariac.log --listen-port=16999 --dht-listen-port=16999
 python clean_up.py
