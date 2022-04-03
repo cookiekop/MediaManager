0656fdd4c62606927a3f3920ac8ef4319c1c98a4
cookiekop <michaelzhang1234@gmail.com>
2022-04-03 13:51:14 +0800

    Update: filter bad torrents.

--- get_magnet_url.py
+++ get_magnet_url.py
@@ -14,7 +14,10 @@ def main():
                         api.RarbgAPI.CATEGORY_MOVIE_X264_720P, \
                         api.RarbgAPI.CATEGORY_MOVIE_X265_1080P]
     series_categories = [api.RarbgAPI.CATEGORY_TV_EPISODES_HD]
-    db_data = db.query(db_tb_name, "(rating=0 OR rating=5) AND jf_id is NULL AND is_series=0")[:max_downloads]
+    db_data = db.query(db_tb_name, "rating=0 AND jf_id is NULL AND is_series=0")[:max_downloads]
+    if len(db_data) < max_downloads: db_data.extend(db.query(db_tb_name, "rating=0 AND jf_id is NULL AND is_series=0") \
+                                     [:max_downloads - len(db_data)])
+                                     
     with open("magnets_movies.txt", "w") as f_l_m, \
          open("magnets_series.txt", "w") as f_l_s, \
          open("filenames.txt", "w") as f_n:
@@ -25,8 +28,11 @@ def main():
             for retry in range(retries):
                 api_ret = client.search(search_imdb=data[0], categories=category, sort='last')
                 if len(api_ret) > 0: 
-                    f_n.write(fn_prefix+"/"+api_ret[0].filename+"\n")
-                    f.write(api_ret[0].download+"\n")
+                    for torrent in api_ret:
+                        if torrent.leecher == 0 and torrent.seeder < 3: continue
+                        f_n.write(fn_prefix+"/"+torrent.filename+"\n")
+                        f.write(torrent.download+"\n")
+                        break
                     break
                 if (retry == retries - 1): handler.notify("RARBG Max Retries")
     print(download_dir)
